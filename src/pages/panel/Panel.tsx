0143368d12886b09963b680b27470d943ec258a2
Kevin Gonzalez <19812642+typekev@users.noreply.github.com>
2024-12-27 03:19:51 +0100

    Lint fixes

--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import '@pages/panel/Panel.css';
 
 export default function Panel() {
--- src/pages/panel/index.tsx
+++ src/pages/panel/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+import '@assets/styles/tailwind.css';
 import Panel from '@pages/panel/Panel';
 import '@pages/panel/index.css';
-import '@assets/styles/tailwind.css';
 
 function init() {
-  const rootContainer = document.querySelector("#__root");
+  const rootContainer = document.querySelector('#__root');
   if (!rootContainer) throw new Error("Can't find Panel root element");
   const root = createRoot(rootContainer);
   root.render(<Panel />);
